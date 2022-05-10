a72b473b8cf807bfcbc91733fd152bdc9a27802f
Guilherme Job <guilhermejob@hotmail.com>
2022-05-10 14:49:29 -0300

    added first test

---
+++ lambdas/customers/index.unit.test.ts
@@ -0,0 +1,8 @@
+
+describe("#customers", function () {
+    describe("get()", function () {
+        it(`my first test`, function () {
+            expect(true).toEqual(true);
+        });
+    });
+});
