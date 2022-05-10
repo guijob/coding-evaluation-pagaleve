cc45c2fd2a2196bcee98f438ec112cb0ecc4e895
Guilherme Job <guilhermejob@hotmail.com>
2022-05-10 20:20:13 -0300

    first lambda implementation

--- functions/customers/index.ts
+++ functions/customers/index.ts
@@ -1,7 +1,16 @@
-export default function (event, context, callback) {
-    return callback(null, {
-        statusCode: 200,
-        body: 'Hello!',
-        headers: { 'Content-Type': 'text/plain' }
-    });
-};
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+const supportedEndpoints = [];
+
+export default async function (event: APIGatewayProxyEvent) {
+    const { parkingLotId } = event.pathParameters;
+    const requesterUser = JSON.parse(event.requestContext.authorizer.user);
+    const { initialDateTime, finalDateTime } =
+        event.queryStringParameters || {};
+
+    console.info(
+        `endpoint called by ${requesterUser.id}: ${event.httpMethod} ${event.path}`
+    );
+
+    return event;
+}
