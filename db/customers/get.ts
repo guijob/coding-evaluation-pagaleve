6876265b928285bc3949b27ed935ac1f5c853696
Guilherme Job <guilhermejob@hotmail.com>
2022-05-11 15:21:18 -0300

    fixed dynamodb resource region

--- db/customers/get.ts
+++ db/customers/get.ts
@@ -1,7 +1,7 @@
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import { Customer } from "../../models/Customer";
 
-const documentClient = new DocumentClient({ region: "sa-east-1" });
+const documentClient = new DocumentClient({ region: "us-east-1" });
 const customersTable = process.env.CustomersTable;
 
 export default async function get(
