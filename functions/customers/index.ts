b106480f4bf7a18bdfce621b57a90d8d336ae14d
Guilherme Job <guilhermejob@hotmail.com>
2022-05-11 13:22:52 -0300

    returning event obj

--- functions/customers/index.ts
+++ functions/customers/index.ts
@@ -1,16 +1,32 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 const supportedEndpoints = [];
+
+const documentClient = new DocumentClient({ region: 'sa-east-1' });
+const customersTable = process.env.CustomersTable;
+
 
 export default async function (event: APIGatewayProxyEvent) {
     const { parkingLotId } = event.pathParameters;
     const requesterUser = JSON.parse(event.requestContext.authorizer.user);
-    const { initialDateTime, finalDateTime } =
-        event.queryStringParameters || {};
 
     console.info(
         `endpoint called by ${requesterUser.id}: ${event.httpMethod} ${event.path}`
     );
 
-    return event;
+    return done(200, event, null);
 }
+
+const done = (statusCode, res, err) => {
+    return {
+        statusCode: statusCode,
+        body: err
+            ? JSON.stringify({ error: err.message })
+            : JSON.stringify(res),
+        headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+    };
+};
